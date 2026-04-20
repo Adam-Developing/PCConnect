91c43084aefbec637318b68b732aca9d1e700253
Adam Developing <18khattaba@gmail.com>
2026-04-20 22:16:20 +0100

    Delete PCClient directory

--- PCClient/V3/PCClient/ReminderData.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ReminderData
-    Public Property ID As Integer
-    Public Property Username As String
-    Public Property [Date] As String
-    Public Property Time As String
-    Public Property Reminder As String
-    Public Property Completed As Integer
-End Class
--- PCClient/V4.5/PCClient/auto-run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\Program Files\PCClient"
-start "" "C:\Program Files\PCClient\PCClient.exe"
