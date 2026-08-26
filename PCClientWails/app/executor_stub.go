38886d2eafc98146c46498c1c1a62327a3a896db
Adam Khattab <18khattaba@gmail.com>
2026-08-26 14:53:31 +0100

    Latest released version ready for upgrading

--- PCClientWails/app/executor_stub.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package app
-
-import "fmt"
-
-func executeCommand(command string) (string, error) {
-	return "", fmt.Errorf("unsupported OS for command execution: %s", command)
-}
